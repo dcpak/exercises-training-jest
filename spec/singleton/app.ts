03c48fcfc62af7034cfe39450b14235db536489e
Tomasz Ducin <tomasz@ducin.it>
2020-04-14 13:53:52 +0200

    singleton update

--- spec/singleton/app.ts
+++ spec/singleton/app.ts
@@ -1,5 +1,13 @@
 import { produceSingleton } from "./singleton-fp";
 
 export const i18nConfig = produceSingleton()
+i18nConfig.getInstance()
 export const themeConfig = produceSingleton()
 export const filteringConfig = produceSingleton()
+
+import { SingletonFactory } from './singleton-oop'
+
+export const _i18nConfig = SingletonFactory.produce
+_i18nConfig().getInstance()
+export const _themeConfig = SingletonFactory.produce()
+export const _filteringConfig = SingletonFactory.produce()
--- spec/singleton/singleton-oop.ts
+++ spec/singleton/singleton-oop.ts
@@ -21,7 +21,7 @@ export class Singleton {
 
 
 
-class SingletonFactory {
+export class SingletonFactory {
   static produce(){
     // anonymous class
     return class {
